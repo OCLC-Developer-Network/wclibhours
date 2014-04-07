34eacaf6e49eefaada99b694ec047071c6fc5e0b
Karen Coombs <librarywebchic@gmail.com>
2014-04-07 13:26:57 -0500

    Added helpers php functions to bootstrap

--- WorldCatRegistryHoursDemo/tests/InstitutionTest.php
+++ WorldCatRegistryHoursDemo/tests/InstitutionTest.php
@@ -9,7 +9,7 @@ Class InstitutionTest extends \PHPUnit_Framework_TestCase {
 		EasyRdf_Namespace::set('schema', 'http://schema.org/');
         EasyRdf_Namespace::set('wcir', 'http://purl.org/oclc/ontology/wcir/');
         EasyRdf_TypeMapper::set('schema:Organization', 'OCLC\WorldCatRegistryDemo\Models\Institution');
-        EasyRdf_TypeMapper::set('wcir:hoursSpecification', 'HoursSpec');
+        EasyRdf_TypeMapper::set('wcir:hoursSpecification', 'OCLC\WorldCatRegistryDemo\Models\HoursSpec');
         $this->graph = new EasyRdf_Graph();
         $this->graph->parseFile("sample-data/organization.rdf");
         $this->graph->parseFile("sample-data/normal-hours.rdf");
--- WorldCatRegistryHoursDemo/tests/bootstrap.php
+++ WorldCatRegistryHoursDemo/tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 error_reporting(E_ALL | E_STRICT);
 require '../vendor/autoload.php';
+require '../app/helpers/application_helper_typemapped.php';
 
 //load yaml file
 global $config;
