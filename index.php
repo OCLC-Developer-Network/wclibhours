5aee694aa76d67838a209d618808f4fba5800756
Karen Coombs <librarywebchic@gmail.com>
2014-04-09 10:58:23 -0500

    Removed load of static file from index.php

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <?php
 require 'vendor/autoload.php';
 
-global $config;
-$config = Yaml::parse('../app/config/config.yaml');
-
+use Symfony\Component\Yaml\Yaml;
 use Models\Institution;
 use Models\HoursSpec;
+
+global $config;
+$config = Yaml::parse('app/config/config.yaml');
 
 EasyRdf_Namespace::set('schema', 'http://schema.org/');
 EasyRdf_Namespace::set('wcir', 'http://purl.org/oclc/ontology/wcir/');
 EasyRdf_TypeMapper::set('schema:Organization', 'OCLC\WorldCatRegistryDemo\Models\Institution');
 EasyRdf_TypeMapper::set('wcir:hoursSpecification', 'OCLC\WorldCatRegistryDemo\Models\HoursSpec');
 
-// application/json is not RDF in our world so need to unregister it
+//unregister application/json because it is not RDF
 EasyRdf_Format::unregister('json');
 
 $graph = new EasyRdf_Graph();
-// $graph->load('https://worldcat.org/wcr/organization/resource/' . $config['institution']);
-$graph->parseFile("tests/sample-data/organization.rdf");
+$graph->load('https://worldcat.org/wcr/organization/resource/' . $config['institution']);
 
 $orgs = $graph->allOfType('schema:Organization');
 $org = $orgs[0];
