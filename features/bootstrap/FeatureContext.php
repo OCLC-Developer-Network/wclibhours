6b1fd3789ab2042af928963a063779dafc470fd7
Karen Coombs <librarywebchic@gmail.com>
2014-07-23 20:04:43 -0500

    Added FeatureContext, and behat configuration file

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,53 @@
+<?php
+
+use Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\TranslatedContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+use Behat\MinkExtension\Context\RawMinkContext;
+use Behat\MinkExtension\Context\MinkContext;
+
+//
+// Require 3rd-party libraries here:
+//
+   require_once 'PHPUnit/Autoload.php';
+   require_once 'PHPUnit/Framework/Assert/Functions.php';
+//
+
+/**
+ * Features context.
+ */
+class FeatureContext extends RawMinkContext
+{
+    /**
+     * Initializes context.
+     * Every scenario gets it's own context object.
+     *
+     * @param array $parameters context parameters (set them up through behat.yml)
+     */
+    public function __construct(array $parameters)
+    {
+        $this->useContext('mink', new MinkContext);
+    }
+
+    /**
+     * @Then /^I should see the following in the repeated "([^"]*)" element within the context of the "([^"]*)" element:$/
+     */
+    public function assertRepeatedElementContainsText($element, $parentElement, TableNode $table)
+    {
+        $elementSelector = $parentElement . ' ' . $element;
+        $elements = $this->getSession()->getPage()->findAll('css', $elementSelector);
+    
+        foreach ($table->getHash() as $n => $repeatedElement) {
+            $singleElements = $elements[$n];
+    
+            \PHPUnit_Framework_Assert::assertEquals(
+                $singleElements->getText(),
+                $repeatedElement['text']
+            );
+        }
+    }
+}
